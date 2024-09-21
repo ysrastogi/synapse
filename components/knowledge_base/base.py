5ada668fb5ec3be25b5427af25f74839b5292822
ysrastogi <ysrastogi19@gmail.com>
2024-09-22 02:45:43 +0530

    CREATED: node publisher

--- components/knowledge_base/base.py
+++ components/knowledge_base/base.py
@@ -1,13 +1,23 @@
 from qdrant_client.models import Distance, VectorParams, PointStruct, Filter, FieldCondition, MatchValue
 from connections.db import VectorDB
+from connections.pubsub import Publisher
+import uuid
 
 class KnowledgeBaseFunc(VectorDB):
-
+    def __init__(self):
+        super().__init__()
+        self.service_id = 'knowledge_base'+str(uuid.uuid4())
+        self.publisher = Publisher('knowledge_base_updates', self.service_id)
+    
+    def get_service_id(self):
+        return self.service_id
+    
     def create_collection(self, collection_name: str, vector_size: int, distance: Distance) -> None:
             self.client.create_collection(
             collection_name=collection_name,
             vectors_config=VectorParams(size=vector_size, distance=distance),
             )
+            self.publisher.publish(f"Collection {collection_name} created with vector size {vector_size} and distance {distance}")
 
     def upsert(self, collection_name: str, points: list[PointStruct], wait: bool = True) -> dict:
         operation_info = self.client.upsert(
@@ -15,12 +25,14 @@ class KnowledgeBaseFunc(VectorDB):
             wait=wait,
             points=points,
         )
+        self.publisher.publish(f"Upserted points into collection {collection_name}")
         return operation_info
 
     def search(self, collection_name: str, query: list[float], limit: int) -> list:
         search_result = self.client.query_points(
             collection_name=collection_name, query=query, limit=limit
         ).points
+        self.publisher.publish(f"Searched in collection {collection_name} with query {query} and limit {limit}")
         return search_result
 
     def search_with_filter(self, collection_name: str, query: list[float], filter_key: str, filter_value: str, limit: int) -> list:
@@ -33,6 +45,7 @@ class KnowledgeBaseFunc(VectorDB):
             with_payload=True,
             limit=limit,
         ).points
+        self.publisher.publish(f"Searched in collection {collection_name} with query {query}, filter {filter_key}={filter_value}, and limit {limit}")
         return search_result
     
     def neural_searcher(self, collection_name: str, model=None, embeddings=None, text:str= None):
@@ -49,4 +62,5 @@ class KnowledgeBaseFunc(VectorDB):
             limit=2
         )
         payloads = [hit.payload for hit in search_result]
+        self.publisher.publish(f"Neural searcher collection {collection_name} with text '{text}'")
         return payloads
